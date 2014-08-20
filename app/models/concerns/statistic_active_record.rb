41efc844fe4c809f06c6fdfc2c2a605066cc9696
Aleksey Zolotov <myxrome@gmail.com>
2014-08-20 22:15:22 +0400

    конвертация строки подключения к бд в символ, вместо строки (строгий варнинг выдавался).

--- app/models/concerns/statistic_active_record.rb
+++ app/models/concerns/statistic_active_record.rb
@@ -1,4 +1,4 @@
 class StatisticActiveRecord < ActiveRecord::Base
   self.abstract_class = true
-  establish_connection "statistic_#{Rails.env}"
+  establish_connection "statistic_#{Rails.env}".to_sym
 end
