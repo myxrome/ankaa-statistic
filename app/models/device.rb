cb7ab9c4e14ca8ea87f1983f8ed4fdb940d4ac24
Aleksey Zolotov <myxrome@gmail.com>
2014-08-25 22:44:12 +0400

    возможность создания сессий из под девайсов и фактов из под сессий.

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,5 @@
 class Device < StatisticActiveRecord
   has_many :sessions, inverse_of: :device
+  accepts_nested_attributes_for :sessions, reject_if: :all_blank, allow_destroy: true
+
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,6 @@
 class Session < StatisticActiveRecord
   belongs_to :device, inverse_of: :sessions
   has_many :facts, inverse_of: :session
+  accepts_nested_attributes_for :facts, reject_if: :all_blank, allow_destroy: true
 
 end
